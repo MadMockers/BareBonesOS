17b09ca47b087fdcd47943418a2de9ae59fc7dfe
Glen Chatfield <glenchatfield@xegup.com>
2015-09-05 10:19:25 +1000

    Bootloader is now position independant
    Now loads itself directly below BBOS in memory

--- src/bootloader.asm
+++ src/bootloader.asm
@@ -1,45 +1,53 @@
 
 #include "bbos.inc.asm"
 
-.define BL_SIZE end-loader_entry
+.define BL_SIZE loader_end-loader_entry
 
 copy_start:
 start:
     PUSH A  ; A has drive to load off
+        ; get position of BBOS
+        SET A, 0x0000
+        SUB SP, 4
+        SET B, SP
+        PUSH B
+            INT BBOS_IRQ_MAGIC
+        ADD SP, 1
         SET I, physical_start
-        SET J, loader_entry
+        SET J, [B+BBOS_START_ADDR]
+        SUB J, BL_SIZE
+        SET Z, J
         SET A, BL_SIZE
+        ADD SP, 4
 .copy_top:
         SUB A, 1
         STI [J], [I]
         IFN A, 0
             SET PC, .copy_top
-        SET PC, loader_entry
+        SET PC, Z
 copy_end:
 
 physical_start:
 
-.org 509
-sector_start:
-.org 510
-sector_end:
-.org 511
-magic:
-
-.org 0xE000
 loader_entry:
+    SET J, PC
     POP B
-    PUSH title_str
-        JSR write_screen
+
+    PUSH J
+    ADD [SP], title_str-loader_entry-1
+        SET I, J
+        ADD I, write_screen-loader_entry-1
+        JSR I
     ADD SP, 1
 
     SET A, 0x2003
     SET C, [sector_start]
     SET X, 0
     SET Z, [sector_end]
-.load_top:
+load_top:
     IFE Z, 0
-        SET PC, done
+        ADD PC, done-src1
+src1:
     PUSH C
     PUSH X
     PUSH B
@@ -47,16 +55,23 @@ loader_entry:
     POP Y
     ADD SP, 2
     IFE Y, 0
-        SET PC, load_fail
+        ADD PC, load_fail-src2
+src2:
     SUB Z, 1
     ADD C, 1
     ADD X, 0x200
-    SET PC, .load_top
+    ADD PC, load_top-src3
+src3:
 
 load_fail:
-    PUSH drive_fail
-        JSR write_screen
-        SET PC, die
+    PUSH J
+    ADD [SP], drive_fail-loader_entry-1
+        SET I, J
+        ADD I, write_screen-loader_entry-1
+        JSR I
+    ADD SP, 1
+    ADD PC, die-src4
+src4:
 
 done:
     SET A, B
@@ -81,3 +96,10 @@ title_str:
     .asciiz "BootLoader v0.1"
 loader_end:
 end:
+
+.org 509
+sector_start:
+.org 510
+sector_end:
+.org 511
+magic:
