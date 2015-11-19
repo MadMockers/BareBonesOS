327aba42cbb3c3e65e0554f4a32133ff9e235c90
Glen Chatfield <glenchatfield@xegup.com>
2015-11-19 18:22:36 +1100

    Clarified build_floppy out size

--- util/floppy.h
+++ util/floppy.h
@@ -10,7 +10,7 @@
  * @param bootloader_len    length of bootloader buffer (IN BYTES)
  * @param usr_img           pointer to user provided image to be wrapped
  * @param img_len           length of user image (IN BYTES)
- * @param out_len           pointer to variable that will hold the size of the output
+ * @param out_len           pointer to variable that will hold the size of the output (IN BYTES)
  * @return                  pointer to floppy buffer (must be free'd)
  */
 void *build_floppy(size_t sector_size, void *bootloader, size_t bootloader_len,
